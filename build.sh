7571f465dffa4cd91caac0a4822313525aeca2d7
SukkaW <isukkaw@gmail.com>
2018-07-26 14:52:50 +0800

    feat/ci: log output

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 echo ''
 echo '-------------------------------------'
-echo 'Start Test...'
+echo 'Test _config.example.yml...'
 echo '-------------------------------------'
-echo ''
-echo "** Check example theme config... **"
 echo ''
 \cp -f themes/suka/_config.example.yml themes/suka/_config.yml
 hexo clean --silent
 hexo g
-echo "** Check demo theme config... **"
+echo ''
+echo '-------------------------------------'
+echo 'Build demo site...'
+echo '-------------------------------------'
+echo ''
 \cp -f ./theme_config.yml themes/suka/_config.yml
 hexo clean --silent
 hexo g && gulp
