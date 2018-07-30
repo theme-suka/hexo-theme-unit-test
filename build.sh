e9fa30d1f5a3fc06a3cca260d2cb5bd363e413b5
SukkaW <isukkaw@gmail.com>
2018-07-30 11:20:40 +0800

    Use npm script instead of cli

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ echo 'Test _config.example.yml...'
 echo '-------------------------------------'
 echo ''
 \cp -f themes/suka/_config.example.yml themes/suka/_config.yml
-hexo clean --silent
-hexo g
+npm run clean
+npm run generate
 echo ''
 echo '-------------------------------------'
 echo 'Build demo site...'
 echo '-------------------------------------'
 echo ''
 \cp -f ./theme_config.yml themes/suka/_config.yml
-hexo clean --silent
-hexo g && gulp
+npm run clean
+npm run build
