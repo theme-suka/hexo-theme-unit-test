5309559e07d4604fb60ac2d5b8d61388debca790
SukkaW <isukkaw@gmail.com>
2018-07-30 14:45:58 +0800

    update theme config to latest

--- install-theme.sh
+++ install-theme.sh
@@ -5,6 +5,7 @@ echo 'Install Suka Theme...'
 echo '-------------------------------------'
 echo ''
 git clone https://github.com/SukkaW/hexo-theme-suka.git themes/suka
+git clone --quiet https://github.com/SukkaW/hexo-theme-suka.git themes/suka
 cd themes/suka
 git checkout -t origin/canary
 git pull
