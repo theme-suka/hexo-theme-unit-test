8735808cd705e2aa2ab5cb765baedfb30c12d1e5
SukkaW <isukkaw@gmail.com>
2018-10-29 16:40:41 +0800

    Bring up performance test of Hexo highlight.js

--- perf-test.sh
+++ perf-test.sh
@@ -9,6 +9,7 @@ cd source/_posts/
 git clone https://github.com/SukkaLab/hexo-5000-posts.git --depth=1
 cd ..
 cd ..
+
 # Disable fragment cache
 echo ''
 echo '-------------------------------------'
@@ -19,6 +20,7 @@ npm run clean > /dev/null
 sed -i "s|fragment_cache: true|fragment_cache: false|g" themes/suka/_config.yml
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
+
 # Enable fragment cache
 echo ''
 echo '-------------------------------------'
@@ -27,5 +29,24 @@ echo '-------------------------------------'
 echo ''
 npm run clean > /dev/null
 sed -i "s|fragment_cache: false|fragment_cache: true|g" themes/suka/_config.yml
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+# Disable Hexo Highlight.js
+echo ''
+echo '-------------------------------------'
+echo 'Test performance: 300 posts with fragment_cache enabled and highlight.js disabled'
+echo '-------------------------------------'
+echo ''
+npm run clean > /dev/null
+
+sed -i '40,44d' _config.yml
+echo '
+highlight:
+  enable: false
+  line_number: false
+  auto_detect: false
+  tab_replace: false' >> _config.yml
+
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
