11da4c633ca0ecf935e302d341218eeacacc38ff
Sukka <github@skk.moe>
2018-12-24 21:29:50 +0800

    No longer use Yarn for install theme

--- install-theme.sh
+++ install-theme.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo ''
-echo '-------------------------------------'
-echo 'Install Suka Theme...'
-echo '-------------------------------------'
-echo ''
+echo -n '
+-------------------------------------
+Install Suka Theme...
+-------------------------------------
+'
 mkdir -p ./themes/suka
 cp -rf ../hexo-theme-suka/* ./themes/suka/
 cd ./themes/suka
-yarn install --production > /dev/null
+npm install --production > /dev/null
 cd ..
 cd ..
